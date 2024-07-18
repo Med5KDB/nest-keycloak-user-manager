407b1c2c38e52d78d80ff9e84b91b2acdcc12947
Med5KDB <kevindebruyne17med@gmail.com>
2024-07-18 00:50:28 +0000

    make the kc admin client provider, the kc user manager service & controllers available to any module that imports KeycloakUserManagerModule add try/catch and logger for better errors handling

--- src/keycloak-user-manager/keycloak-user-manager.controller.ts
+++ src/keycloak-user-manager/keycloak-user-manager.controller.ts
@@ -1,8 +1,10 @@
 import {
   Body,
+  ConflictException,
   Controller,
   Delete,
   Get,
+  InternalServerErrorException,
   Param,
   Post,
   Put,
@@ -16,15 +18,29 @@ import {
   UserWhereUniqueInput,
 } from 'src/dto/keycloak-user-manager.dto';
 import { ResultWithCount } from 'src/dto/result-with-count.dto';
+import { Logger } from '@nestjs/common';
 
 @Controller('users')
 export class KeycloakUserManagerController {
+  private readonly logger = new Logger(KeycloakUserManagerController.name);
   constructor(
     private readonly keycloakUserManagerService: KeycloakUserManagerService,
   ) {}
   @Post()
   async createUser(@Body() data: UserCreateInput): Promise<User> {
-    return this.keycloakUserManagerService.createUser(data);
+    try {
+      return this.keycloakUserManagerService.createUser(data);
+    } catch (err) {
+      if (err instanceof ConflictException) {
+        throw new ConflictException(
+          `User with username or email already exists`,
+        );
+      }
+      this.logger.error(err);
+      throw new InternalServerErrorException(
+        `User creation failed due to ${err}`,
+      );
+    }
   }
 
   @Put(':id')
@@ -32,33 +48,71 @@ export class KeycloakUserManagerController {
     @Body() data: UserUpdateInput,
     @Param('id') where: UserWhereUniqueInput,
   ): Promise<User> {
-    const updatedUser = this.keycloakUserManagerService.updateUser(where, data);
-    return updatedUser;
+    try {
+      const updatedUser = this.keycloakUserManagerService.updateUser(
+        where,
+        data,
+      );
+      return updatedUser;
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(
+        `Failed to update user due to ${err}`,
+      );
+    }
   }
 
   @Delete(':id')
   async deleteUser(
     @Param('id') where: UserWhereUniqueInput,
   ): Promise<{ id: string }> {
-    return this.keycloakUserManagerService.deleteUser(where);
+    try {
+      return this.keycloakUserManagerService.deleteUser(where);
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(
+        `Failed to delete user due to ${err}`,
+      );
+    }
   }
 
   @Get()
   async getUsers(): Promise<ResultWithCount<User>> {
-    const [users, count] = await Promise.all([
-      this.keycloakUserManagerService.findManyUsers(),
-      this.keycloakUserManagerService.countUsers(),
-    ]);
-    return { data: users, count };
+    try {
+      const [users, count] = await Promise.all([
+        this.keycloakUserManagerService.findManyUsers(),
+        this.keycloakUserManagerService.countUsers(),
+      ]);
+      return { data: users, count };
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(
+        `Failed to get users due to ${err}`,
+      );
+    }
   }
 
   @Get(':id')
   async getUser(@Param('id') where: UserWhereUniqueInput): Promise<User> {
-    return this.keycloakUserManagerService.findUserById(where);
+    try {
+      return this.keycloakUserManagerService.findUserById(where);
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(
+        `Failed to get user due to ${err}`,
+      );
+    }
   }
 
   @Put('reset-password')
   async resetUserPassword(@Body() data: ResetUserPasswordInput) {
-    return this.keycloakUserManagerService.resetUserPassword(data);
+    try {
+      return this.keycloakUserManagerService.resetUserPassword(data);
+    } catch (err) {
+      this.logger.error(err);
+      throw new InternalServerErrorException(
+        `Failed to reset user password due to ${err}`,
+      );
+    }
   }
 }
--- src/keycloak-user-manager/keycloak-user-manager.module.ts
+++ src/keycloak-user-manager/keycloak-user-manager.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { KeycloakUserManagerService } from './keycloak-user-manager.service';
 import { KeycloakUserManagerController } from './keycloak-user-manager.controller';
+import KeycloakAdminClient from '@keycloak/keycloak-admin-client';
 
 @Module({
   controllers: [KeycloakUserManagerController],
-  providers: [KeycloakUserManagerService],
+  providers: [
+    KeycloakUserManagerService,
+    KeycloakUserManagerController,
+    KeycloakAdminClient,
+  ],
+  exports: [KeycloakUserManagerService],
 })
 export class KeycloakUserManagerModule {}
