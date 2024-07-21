8d0cee7ab9e5ae4a67a3f7be5b633ba30b2d8121
Med5KDB <kevindebruyne17med@gmail.com>
2024-07-21 12:57:44 +0000

    fix: correct module provider to use KCAdminClientProvider instead of KeycloakAdminClient

--- src/keycloak-user-manager/keycloak-user-manager.module.ts
+++ src/keycloak-user-manager/keycloak-user-manager.module.ts
@@ -1,17 +1,17 @@
 import { DynamicModule, Global, Module } from '@nestjs/common';
 import { KeycloakUserManagerService } from './keycloak-user-manager.service';
 import { KeycloakUserManagerController } from './keycloak-user-manager.controller';
-import KeycloakAdminClient from '@keycloak/keycloak-admin-client';
 import {
   ConfigurableModuleClass,
   MODULE_OPTIONS_TOKEN,
 } from 'src/kc-user-manager.module-definition';
 import { KeycloakUserManagerModuleConfigOptions } from 'src/kc-user-manager.module-config-options';
+import { KCAdminClientProvider } from 'src/providers/kc-admin-client.provider';
 
 @Global()
 @Module({
   controllers: [KeycloakUserManagerController],
-  providers: [KeycloakUserManagerService, KeycloakAdminClient],
+  providers: [KeycloakUserManagerService, KCAdminClientProvider],
   exports: [KeycloakUserManagerService],
 })
 export class KeycloakUserManagerModule extends ConfigurableModuleClass {
